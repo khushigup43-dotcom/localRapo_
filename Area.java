ca9b7d5b628dfad009705e19821189f959114d2f
Khushi <khushi.gup43@gmail.com>
2026-04-18 09:52:59 +0530

    add initial files

---
+++ Area.java
@@ -0,0 +1,9 @@
+package localRapo;
+
+public class Area {
+   public static void main(String[] args) {
+        double radius = 5.0;
+        double area = Math.PI * radius * radius;
+        System.out.println("The area of a circle with radius " + radius + " is: " + area);
+    } 
+}
---
+++ Difference.java
@@ -0,0 +1,11 @@
+package localRapo;
+
+public class Difference {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int difference = a - b;
+        System.out.println("The difference between " + a + " and " + b + " is: " + difference);
+    }
+    
+}
