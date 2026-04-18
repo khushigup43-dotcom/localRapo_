fdb6c5a773eb215249186b0d0f994410b72ad5b0
Khushi <khushi.gup43@gmail.com>
2026-04-18 10:26:37 +0530

    add new feature

--- Area.java
+++ Area.java
@@ -2,8 +2,11 @@ package localRapo;
 
 public class Area {
    public static void main(String[] args) {
+        double length = 4.0;
         double radius = 5.0;
-        double area = Math.PI * radius * radius;
-        System.out.println("The area of a circle with radius " + radius + " is: " + area);
+        double area = length * length;         
+        double a1 = Math.PI * radius * radius;
+        System.out.println("The area of a square with side " + length + " is: " + area);
+        System.out.println("The area of a circle with radius " + radius + " is: " + a1);
     } 
 }
